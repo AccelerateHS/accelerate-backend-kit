79550776948692f09e35e6eb9acc3899510d1862
Ryan Newton <rrnewton@gmail.com>
2013-03-28 00:47:57 -0400

    Push the refactor through UnzipArrays/doE, but still need to finish one piece

--- backend-kit/Data/Array/Accelerate/BackendKit/Phase2/UnzipArrays.hs
+++ backend-kit/Data/Array/Accelerate/BackendKit/Phase2/UnzipArrays.hs
@@ -71,7 +71,7 @@ doBinds env (ProgBind vo aty (SubBinds {subnames,arrsize},d2)
 doBinds env (ProgBind vo ty dec@(SubBinds {subnames},_) op : rest) = do
   (dec',op') <-
       case op of
-        Left  ex -> do ex' <- return$ doE env ex
+        Left  ex -> do ex' <- doE env ex
                        return (OpInputs[], Left ex')
         Right ae -> do (ls,ae') <- doAE env ae
                        return (OpInputs ls,Right ae')
@@ -83,36 +83,44 @@ doAE :: Env -> AExp -> GensymM ([[Var]], AExp)
 doAE env ae = 
   case ae of
     Use _               -> return ([],ae)
-    Cond a b c          -> return ([sp b,sp c], Cond (exp a) nukedVar nukedVar)
+    Cond a b c          -> do a' <- (exp a)
+                              return ([sp b,sp c], Cond a' nukedVar nukedVar)
     Generate e lam1     -> do l1 <- doLam1 lam1
-                              return ([], Generate (exp e) l1)
+                              e' <- (exp e)
+                              return ([], Generate e' l1)
     Fold lam2 e v       -> do l2 <- doLam2 lam2
-                              return ([sp v],Fold l2 (exp e) nukedVar)
+                              e' <- (exp e)
+                              return ([sp v],Fold l2 e' nukedVar)
     Fold1    lam2 v     -> do l2 <- doLam2 lam2
                               return ([sp v],Fold1 l2 nukedVar)
     FoldSeg  lam2 e v w -> do l2 <- doLam2 lam2
-                              return ([sp v,sp w],FoldSeg l2 (exp e) nukedVar nukedVar)
+                              e' <- (exp e)
+                              return ([sp v,sp w],FoldSeg l2 e' nukedVar nukedVar)
     Fold1Seg lam2 v w   -> do l2 <- doLam2 lam2
                               return ([sp v,sp w],Fold1Seg l2        nukedVar nukedVar)
     Scanl    lam2 e v   -> do l2 <- doLam2 lam2
-                              return ([sp v],Scanl  l2 (exp e) nukedVar)
+                              x  <- (exp e)
+                              return ([sp v],Scanl  l2 x nukedVar)
     Scanl'   lam2 e v   -> do l2 <- doLam2 lam2
-                              return ([sp v],Scanl' l2 (exp e) nukedVar)
+                              e' <- (exp e)
+                              return ([sp v],Scanl' l2 e' nukedVar)
     Scanl1   lam2   v   -> do l2 <- doLam2 lam2
                               return ([sp v],Scanl1 l2         nukedVar)
     Scanr    lam2 e v   -> do l2 <- doLam2 lam2
-                              return ([sp v],Scanr  l2 (exp e) nukedVar)
+                              e' <- (exp e)
+                              return ([sp v],Scanr  l2 e' nukedVar)
     Scanr'   lam2 e v   -> do l2 <- doLam2 lam2
-                              return ([sp v],Scanr' l2 (exp e) nukedVar)
+                              e' <- exp e
+                              return ([sp v],Scanr' l2 e' nukedVar)
     Scanr1   lam2   v   -> do l2 <- doLam2 lam2
                               return ([sp v],Scanr1 l2         nukedVar)
     Stencil  lam1 b v   -> do l1 <- doLam1 lam1
                               return ([sp v],Stencil l1 b nukedVar)
-    Stencil2 l2 b v c w -> do l2 <- (doLam2 l2)
-                              return ([sp v,sp w],Stencil2 l2 b nukedVar c nukedVar)
-    Permute l2 v l1 w   -> do l2 <- (doLam2 l2)
-                              l1 <- (doLam1 l1)
-                              return ([sp v,sp w],Permute l2 nukedVar l1 nukedVar)
+    Stencil2 l2 b v c w -> do l2' <- (doLam2 l2)
+                              return ([sp v,sp w],Stencil2 l2' b nukedVar c nukedVar)
+    Permute l2 v l1 w   -> do l2' <- (doLam2 l2)
+                              l1' <- (doLam1 l1)
+                              return ([sp v,sp w],Permute l2' nukedVar l1' nukedVar)
     Unit {}             -> err ae
     Map  {}             -> err ae
     ZipWith {}          -> err ae
@@ -127,34 +135,34 @@ doAE env ae =
             Just x  -> x
    exp = doE env
    -- We're NOT detupling scalar vars at this point, so we don't bother extending the env:
-   doLam1 (Lam1 (v,ty) bod) = return$ Lam1 (v,ty) (doE env bod)
-   doLam2 (Lam2 b1 b2 bod)  = return$ Lam2 b1 b2 (doE env bod)
+   doLam1 (Lam1 (v,ty) bod) = Lam1 (v,ty) <$> doE env bod
+   doLam2 (Lam2 b1 b2 bod)  = Lam2 b1 b2  <$> doE env bod
 
-doE :: M.Map Var [Var] -> Exp -> Exp
+doE :: M.Map Var [Var] -> Exp -> GensymM Exp
 doE env ex =
   case ex of
     ETupProject ix len (EIndexScalar avr ind) ->
       if len /= 1 then error$"UnzipArrays.hs: ETupProject with len/=1: "++show ex
       else
         maybtrace ("Projecting out of "++show (env # avr)++" for avr "++show avr++" want index "++show ix)$
-        EIndexScalar (reverse (env # avr) !! ix) (doE env ind)
-    EIndexScalar avr e
-      | isTrivialE e ->
-        -- We may be forced to create an ETuple here, but it must be in tail position.
-        -- TODO: AUDIT THIS FURTHER!
-        -- TODO: thread gensym through so we can do MaybeLet here.
-        mkETuple [ EIndexScalar avr' (doE env e) | avr' <- env#avr ]
-      | otherwise -> error$"UnzipArrays.hs: needed refactor here... push through GensymM, nontrivial Exp: "++show e
-    ETupProject ix l e  -> ETupProject ix l (doE env e) 
+        EIndexScalar (reverse (env # avr) !! ix) <$> (doE env ind)
+    EIndexScalar avr e -> 
+      do e' <- doE env e
+         let ty = error "FINISHME -- still need type"
+         -- We may be forced to create an ETuple here, but it must be in tail position.
+         maybeLetE e' ty $ \ e'' -> 
+          mkETuple [ EIndexScalar avr' e'' | avr' <- env#avr ]
+    ETupProject ix l e  -> ETupProject ix l <$> doE env e
     EShape _            -> err ex
     EShapeSize _        -> err ex
     EIndex _            -> err ex
-    EConst _            -> ex
-    EVr _               -> ex
-    ECond e1 e2 e3      -> ECond (doE env e1) (doE env e2) (doE env e3)
-    ELet (v,t,rhs) bod  -> ELet (v,t,doE env rhs) (doE env bod)
-    ETuple els          -> ETuple       (map (doE env) els)
-    EPrimApp p t els    -> EPrimApp p t (map (doE env) els)
+    EConst _            -> return ex
+    EVr _               -> return ex
+    ECond e1 e2 e3      -> ECond <$> doE env e1 <*> doE env e2 <*> doE env e3
+    ELet (v,t,rhs) bod  -> do rhs' <- doE env rhs
+                              ELet (v,t,rhs') <$> doE env bod
+    ETuple els          -> ETuple       <$> mapM (doE env) els
+    EPrimApp p t els    -> EPrimApp p t <$> mapM (doE env) els
  
 
 
