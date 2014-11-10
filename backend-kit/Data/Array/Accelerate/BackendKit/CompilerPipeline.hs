09be1c823bf85ad2c92358d3a48815694535a45d
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2014-11-10 15:52:52 -0500

    re-disable the type checker
    Although ToAccClone is now correctly generating the correct type structure,
    since ETupProject is defined on a flat structure the type gets prematurely
    flattened and hence can not be used for purposes of type checking.

--- backend-kit/Data/Array/Accelerate/BackendKit/CompilerPipeline.hs
+++ backend-kit/Data/Array/Accelerate/BackendKit/CompilerPipeline.hs
@@ -319,10 +319,17 @@ runSAPass1D msg pass input =
 
 optionalTC :: (S.Prog a -> S.Prog a) -> S.Prog a -> S.Prog a
 optionalTC tc prog =
-  -- if dbg > 0
-  -- then trace (" [dbg] Typechecking is TURNED OFF!")
-  --      prog
-  -- else prog
+  -- We have disabled the type-checker (again) because the compiler is
+  -- prematurely flattening tuples. If some expression returns a nested tuple,
+  -- the type checker will (now) know that the expression should be nested, but
+  -- the recovered type (recoverExpType) will be completely flattened because of
+  -- the way that ETupProject is defined. Boom.
+  --
+  if dbg > 0
+  then trace (" [dbg] Typechecking is TURNED OFF!")
+       prog
+  else prog
+{--
   let prog2 = if dbg>0
               then tc prog
               else prog
@@ -331,6 +338,7 @@ optionalTC tc prog =
                  ++", num binds "++show(length (S.progBinds prog)))
           prog2
      else prog2
+--}
 
 --------------------------------------------------------------------------------
 -- Misc:
